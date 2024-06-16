25b79cd883812728d45e3a2484f98642cfc9ab2e
Aaron Mast <amast09@gmail.com>
2024-06-16 14:40:04 -0400

    Implement classic hello world counter UI using Iced library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{button, column, text};
+use iced::{Element, Sandbox, Settings};
+
+struct Counter {
+    value: i64,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum CounterMessage {
+    Decrement,
+    Increment,
+}
+
+impl Sandbox for Counter {
+    type Message = CounterMessage;
+
+    fn new() -> Self {
+        Self { value: 0 }
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter")
+    }
+
+    fn update(&mut self, message: CounterMessage) {
+        match message {
+            CounterMessage::Decrement => {
+                self.value -= 1;
+            }
+            CounterMessage::Increment => {
+                self.value += 1;
+            }
+        }
+    }
+
+    fn view(&self) -> Element<CounterMessage> {
+        column![
+            button("+").on_press(CounterMessage::Increment),
+            text(self.value),
+            button("-").on_press(CounterMessage::Decrement),
+        ].into()
+    }
+}
+
+pub fn main() -> iced::Result {
+    Counter::run(Settings::default())
 }
